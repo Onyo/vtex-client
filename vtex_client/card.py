a8f3c1a7b7d42b34f9c87272784bbb16f8b8cd7f
Ervilis Souza <ervilisviana@gmail.com>
2015-10-29 15:15:08 -0200

    Fixing typo

--- vtex_client/card.py
+++ vtex_client/card.py
@@ -9,7 +9,7 @@ class CardClient(BaseClient):
     def get_information(self, numbers):
         """Get a card information from the first six numbers of the card.
 
-        :param bin: first six numbers of the card
+        :param numbers: first six numbers of the card
         :returns: card information
         """
         return self._make_request("api/pvt/bins?code={}".format(numbers), 'get')
